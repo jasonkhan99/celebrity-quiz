264fb3da7d80559c595def840c9e8bda9cfdc75f
James Henager <jameschenager@gmail.com>
2020-06-04 14:10:32 -0700

    add js logic
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
-  $("form#form").submit(function(){
-    const age = parseInt($("input#age".val());
-    const brad = $("option#brad").val();
-    console.log(brad);
- 
-    if (age < 18) {
-      alert('You are too young for this quiz');
-    } else if (age >= 18) {
+// Business Logic
 
-    } else {
-      alert ("Please enter an age")
-    }
+
+
+
+
+// UI Logic
+$(document).ready(function(){
+  $("form#quiz").submit(function(){
+    const age = parseInt($("input#age").val());
+    var count = $('#brad option:checked').length;
+    window.alert( $(count).val());
+    //const brad = parseInt($("select#brad option:checked").val());
+    console.log(count);
+
+    // if (age < 18) {
+      // alert('You are too young for this quiz');
+    // } else if (age >= 18) {
+
+    // } else {
+    //   alert ("Please enter an age")
+    // }
 
 
   });
