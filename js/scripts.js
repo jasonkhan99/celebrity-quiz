9eabd332591ca9f9bdcf5764151f4667f57828bd
James Henager <jameschenager@gmail.com>
2020-06-04 12:05:11 -0700

    add initial jss logic
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+  $("form#form").submit(function(){
+    const age = parseInt($("input#age".val());
+    const brad = $("option#brad").val();
+    console.log(brad);
+ 
+    if (age < 18) {
+      alert('You are too young for this quiz');
+    } else if (age >= 18) {
 
+    } else {
+      alert ("Please enter an age")
+    }
+
+
+  });
 });
